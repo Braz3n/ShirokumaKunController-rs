13b44b59f0bdd0b48389cb8c38a2d24b2ebaf1f3
Zane Barker <zane.barker@gmail.com>
2025-04-20 01:41:23 +0900

    MQTT is mostly working. Just waiting on definition of aircon commands

--- src/wifi.rs
+++ src/wifi.rs
@@ -1,20 +1,40 @@
-use core::str::from_utf8;
 use cyw43::JoinOptions;
 use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_futures::select::{Either, select};
+use embassy_net::dns::DnsSocket;
 use embassy_net::tcp::TcpSocket;
-use embassy_net::{Config, StackResources};
+use embassy_net::tcp::client::{TcpClient, TcpClientState};
+use embassy_net::{Config, Ipv4Address, StackResources};
 use embassy_rp::clocks::RoscRng;
 use embassy_rp::gpio::Output;
 use embassy_rp::peripherals::{DMA_CH0, PIO0};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Ticker, Timer};
+use embedded_tls::{Aes128GcmSha256, TlsConfig, TlsConnection, TlsContext};
+use heapless::{String, Vec};
 use rand::RngCore;
 use static_cell::StaticCell;
 
+use mqttrs::*;
+
 use crate::ir_tx;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MqttError {
+    #[error("Unexpected packet over tcp")]
+    PacketError,
+    #[error("Connection Failed")]
+    ConnectionError,
+    #[error("Socket Read Error")]
+    SocketReadError,
+    #[error("Socket Write Error")]
+    SocketWriteError,
+}
 
 #[embassy_executor::task]
 async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'static>>) -> ! {
@@ -56,6 +76,30 @@ pub async fn wifi_task(
         .await;
 
     let config = Config::dhcpv4(Default::default());
+    // Use static IP configuration instead of DHCP
+    //let config = embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+    //    address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 69, 2), 24),
+    //    dns_servers: Vec::new(),
+    //    gateway: Some(Ipv4Address::new(192, 168, 69, 1)),
+    //});
+
+    // let ipv6_addr = Ipv6Address::new(0x2405, 0x6580, 0x3460, 0x1100, 0x23, 0x23, 0x23, 0x23);
+    // let ipv6_prefix_len = 64;
+    // let ipv6_cidr = Ipv6Cidr::new(ipv6_addr, ipv6_prefix_len);
+
+    // let ipv6_gateway = Some(Ipv6Address::new(
+    //     0xfe80, 0, 0, 0, 0x7626, 0xacff, 0xfe9c, 0x7dc8,
+    // ));
+
+    // let mut ipv6_dns_servers = Vec::<Ipv6Address, 3>::new();
+    // let _ = ipv6_dns_servers.push(Ipv6Address::new(0x2404, 0x1a8, 0x7f01, 0xa, 0, 0, 0, 0x3));
+    // let _ = ipv6_dns_servers.push(Ipv6Address::new(0x2404, 0x1a8, 0x7f01, 0xb, 0, 0, 0, 0x3));
+    // let ipv6_config = StaticConfigV6 {
+    //     address: ipv6_cidr,
+    //     gateway: ipv6_gateway,
+    //     dns_servers: ipv6_dns_servers,
+    // };
+    // let config = Config::ipv6_static(ipv6_config);
 
     let seed = rng.next_u64();
 
@@ -87,41 +131,140 @@ pub async fn wifi_task(
     }
     info!("DHCP is now up!");
 
+    info!("waiting for stack to be up...");
+    stack.wait_config_up().await;
+    info!("Stack is up!");
+
     let mut rx_buffer = [0; 4096];
     let mut tx_buffer = [0; 4096];
-    let mut buf = [0; 4096];
+    let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+
+    const KEEP_ALIVE_SEC: u16 = 60 * 10; // ten minutes
+    socket.set_timeout(Some(Duration::from_secs(
+        (KEEP_ALIVE_SEC as f32 * 1.2) as u64,
+    )));
+
+    let remote_endpoint = (embassy_net::Ipv4Address::new(10, 0, 1, 15), 1884);
+    log::info!("connecting to {:?}...", remote_endpoint);
+
+    if let Err(e) = socket.connect(remote_endpoint).await {
+        warn!("Socket connection error: {:?}", e);
+        return;
+    }
+    info!("TCP Connected!");
+
+    let mut tcp_read_buffer = [0; 16384];
+    let mut mqtt_buf = [0u8; 1024];
+    mqtt_connect(
+        &mut socket,
+        &mut mqtt_buf,
+        &mut tcp_read_buffer,
+        &KEEP_ALIVE_SEC,
+    )
+    .await
+    .unwrap();
+
+    let mut subscribe_topics = Vec::<SubscribeTopic, 5>::new();
+    let x = SubscribeTopic {
+        topic_path: String::try_from("aircon").unwrap(),
+        qos: QoS::AtLeastOnce,
+    };
+    subscribe_topics.push(x).unwrap();
+
+    mqtt_subscribe(
+        &mut socket,
+        &mut mqtt_buf,
+        &mut tcp_read_buffer,
+        subscribe_topics,
+    )
+    .await
+    .unwrap();
+
+    let mut keep_alive_ticker = Ticker::every(Duration::from_secs(u64::from(
+        KEEP_ALIVE_SEC - (f32::from(KEEP_ALIVE_SEC) * 0.2) as u16,
+    )));
 
     loop {
-        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
-        socket.set_timeout(Some(Duration::from_secs(10)));
+        // https://docs.rs/embassy-futures/latest/embassy_futures/select/fn.select.html
+        // https://github.com/embassy-rs/embassy/blob/443ffd8b6df67844208ceec58d5443e01b99a159/examples/rp/src/bin/orchestrate_tasks.rs#L23
+        // Here we're handling both reception of messages, and maintenance of the keepalive
+        match select(keep_alive_ticker.next(), socket.read(&mut tcp_read_buffer)).await {
+            Either::First(_) => {
+                // Keep alive
+                mqtt_pingreq(&mut socket).await;
+            }
+            Either::Second(_) => {
+                // Handle any other kind of incoming packet
+                match decode_slice(&mut tcp_read_buffer) {
+                    Ok(Some(Packet::Pingresp)) => {
+                        // Ping response
+                        info!("Received a pingresp!");
+                    }
+                    Ok(Some(Packet::Publish(packet))) => {
+                        // Response from subscription
+                        // Handle packet from user device
+                        let mut payload_vec = Vec::<u8, 100>::new();
+                        payload_vec.extend_from_slice(&packet.payload).unwrap();
+                        // let payload_string: String<100> = String::new();
+                        let payload_string: String<100> = String::from_utf8(payload_vec).unwrap();
+                        info!(
+                            "Received packet from topic {:?} with payload {:?}",
+                            packet.topic_name, payload_string
+                        );
 
-        control.gpio_set(0, false).await;
-        info!("Listening on TCP:{}...", wifi_tcp_port);
-        if let Err(e) = socket.accept(wifi_tcp_port).await {
-            warn!("Accept error: {:?}", e);
-            continue;
-        }
-
-        info!("Received connection from {:?}", socket.remote_endpoint());
-        control.gpio_set(0, true).await;
-
-        loop {
-            let _ = match socket.read(&mut buf).await {
-                Ok(0) => {
-                    warn!("Read EOF");
-                    break;
+                        match packet.qospid {
+                            QosPid::AtMostOnce => {
+                                // No ack necessary. Do nothing.
+                            }
+                            QosPid::AtLeastOnce(pid) => {
+                                info!("Expecting at least one response");
+                                mqtt_puback(&mut socket, pid).await;
+                            }
+                            QosPid::ExactlyOnce(_pid) => {
+                                // We either get QoS 0, which requires no response, or QoS 2, which
+                                // we don't currently support
+                                warn!("Unsupported QoS level!")
+                            }
+                        }
+                    }
+                    Ok(None) => {
+                        info!("Insufficient data to decode MQTT message");
+                    }
+                    Err(_error) => {
+                        warn!("Failed to decode MQTT message");
+                    }
+                    _ => {
+                        // Unhandled packet types
+                    }
                 }
-                Ok(n) => {
-                    parse_message(from_utf8(&buf[..n]).unwrap(), &aircon_channel).await;
-                    _ = socket.write("OK".as_bytes()).await;
-                }
-                Err(e) => {
-                    warn!("Read error: {:?}", e);
-                    break;
-                }
-            };
+            }
         }
     }
+
+    // Tls
+    // let config = TlsConfig::new().with_server_name("example.com");
+    // let tls = TlsConnection::new(socket, &mut tcp_read_buffer, &mut tcp_write_buffer);
+
+    // tls.open(TlsContext::new(
+    //     &config,
+    //     UnsecureProvider::new::<Aes128GcmSha256>(OsRng),
+    // ))
+    // .await
+    // .expect("Error establishing TLS connection");
+
+    // tls.write_all(encoded).await.expect("Error writing packet");
+    // tls.flush().await.expect("Error flushing packet");
+
+    // let sz = tls
+    //     .read(&mut rx_buffer[..])
+    //     .await
+    //     .expect("error reading data");
+
+    // log::info!("Read {} bytes: {:?}", sz, &rx_buffer[..sz]);
+
+    // loop {
+    //     // Halt
+    // }
 }
 
 async fn parse_message(
@@ -134,8 +277,10 @@ async fn parse_message(
 
     let mode = if message.trim() == "on" {
         ir_tx::ir_cmd_gen::AirconMode::Heating
-    } else {
+    } else if message.trim() == "of" {
         ir_tx::ir_cmd_gen::AirconMode::Off
+    } else {
+        return;
     };
 
     let state = ir_tx::AirconState {
@@ -148,4 +293,113 @@ async fn parse_message(
     };
 
     channel.send(state).await;
+}
+
+async fn mqtt_send_packet(socket: &mut TcpSocket<'_>, packet: Packet<'_>) {}
+
+async fn mqtt_connect(
+    socket: &mut TcpSocket<'_>,
+    mqtt_buffer: &mut [u8; 1024],
+    tcp_read_buffer: &mut [u8; 16384],
+    keep_alive_sec: &u16,
+) -> Result<(), MqttError> {
+    let pkt = Packet::Connect(Connect {
+        protocol: Protocol::MQTT311,
+        keep_alive: *keep_alive_sec,
+        client_id: "pico_test",
+        // We can optionally attempt to reconnect to the last session (saves having to resubscribe, etc.)
+        clean_session: true,
+        last_will: None,
+        username: None,
+        password: None,
+    });
+
+    let len = encode_slice(&pkt, mqtt_buffer).unwrap();
+    if let Err(e) = socket.write(&(mqtt_buffer[..len])).await {
+        warn!("Write error in connection setup {:?}", e);
+    }
+
+    // Receive the connack packet
+    loop {
+        socket.read(tcp_read_buffer).await.unwrap();
+
+        match decode_slice(tcp_read_buffer) {
+            Ok(Some(Packet::Connack(Connack {
+                session_present: false,
+                code: ConnectReturnCode::Accepted,
+            }))) => {
+                info!("Successfully connected");
+                return Ok(());
+            }
+            Ok(Some(Packet::Connack(_resp))) => {
+                warn!("Unsuccessful connection attempt");
+                return Err(MqttError::ConnectionError);
+            }
+            Ok(None) => info!("Insufficient data to decode MQTT message"),
+            Err(_) => {
+                warn!("Read error in connection setup");
+                return Err(MqttError::SocketReadError);
+            }
+            _ => {
+                warn!("Received unexpected packet over TCP");
+                return Err(MqttError::PacketError);
+            }
+        }
+    }
+}
+
+async fn mqtt_subscribe(
+    socket: &mut TcpSocket<'_>,
+    mqtt_buffer: &mut [u8; 1024],
+    tcp_read_buffer: &mut [u8; 16384],
+    topics: Vec<SubscribeTopic, 5>,
+) -> Result<Vec<SubscribeReturnCodes, 5>, MqttError> {
+    let pid = Pid::new();
+    let pkt = Packet::Subscribe(Subscribe { pid, topics });
+
+    let len = encode_slice(&pkt, mqtt_buffer).unwrap();
+    if let Err(e) = socket.write(&mqtt_buffer[..len]).await {
+        warn!("Write error in connection setup {:?}", e);
+        return Err(MqttError::SocketWriteError);
+    }
+
+    loop {
+        socket.read(tcp_read_buffer).await.unwrap();
+
+        match decode_slice(tcp_read_buffer) {
+            Ok(Some(Packet::Suback(Suback {
+                pid: _,
+                return_codes,
+            }))) => {
+                info!("Successfully subscribed");
+                return Ok(return_codes);
+            }
+            Ok(None) => info!("Insufficient data to decode MQTT message"),
+            Err(_) => {
+                warn!("Read error in connection setup");
+                return Err(MqttError::SocketReadError);
+            }
+            _ => {
+                warn!("Received unexpected packet over TCP");
+                return Err(MqttError::PacketError);
+            }
+        }
+    }
+}
+
+async fn mqtt_pingreq(socket: &mut TcpSocket<'_>) {
+    // We simply cast this out into the void. Reception is handled elsewhere for now.
+    let mut pingreq_buf = [0u8; 2];
+    let _ = encode_slice(&Packet::Pingreq, &mut pingreq_buf).unwrap();
+
+    socket.write(&pingreq_buf).await.unwrap();
+}
+
+async fn mqtt_puback(socket: &mut TcpSocket<'_>, pid: Pid) {
+    // We simply cast this out into the void. Reception is handled elsewhere for now.
+    let mut puback_buf = [0u8; 4];
+    let _ = encode_slice(&Packet::Puback(pid), &mut puback_buf).unwrap();
+    info!("Puback buffer contents: {:?}", puback_buf);
+
+    socket.write(&puback_buf).await.unwrap();
 }
